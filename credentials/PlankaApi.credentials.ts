406c770b63b6b50df5de321e3962404d175f9775
LeGitHubDeTai <65470169+LeGitHubDeTai@users.noreply.github.com>
2026-01-09 16:37:44 +0100

    Fix: credentials

---
+++ credentials/PlankaApi.credentials.ts
@@ -0,0 +1,47 @@
+import {
+    ICredentialTestRequest,
+    ICredentialType,
+    INodeProperties
+} from 'n8n-workflow';
+
+export class PlankaApi implements ICredentialType {
+    name = 'plankaApi';
+    displayName = 'Planka API';
+    documentationUrl = '';
+    properties: INodeProperties[] = [
+        {
+            displayName: 'API URL',
+            name: 'apiUrl',
+            type: 'string',
+            default: '',
+            placeholder: 'https://your-planka-instance.com',
+            required: true,
+            description: 'The URL of your Planka instance API',
+        },
+        {
+            displayName: 'Email',
+            name: 'email',
+            type: 'string',
+            default: '',
+            required: true,
+            description: 'Your Planka account email',
+        },
+        {
+            displayName: 'Password',
+            name: 'password',
+            type: 'string',
+            typeOptions: {
+                password: true,
+            },
+            default: '',
+            required: true,
+            description: 'Your Planka account password',
+        },
+    ];
+    test: ICredentialTestRequest = {
+        request: {
+            baseURL: '={{$credentials?.apiUrl}}',
+            url: '/api/config',
+        },
+    };
+}
